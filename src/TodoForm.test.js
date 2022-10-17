6bd0a36e7e185afa068ffb9c7ee988075bd2962a
hwangj9642 <hwangjj9642@gmail.com>
2022-10-17 15:41:37 +0900

    TodoForm reactoring ok

--- src/TodoForm.test.js
+++ src/TodoForm.test.js
@@ -1,16 +1,31 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render, fireEvent, getAllByPlaceholderText } from "@testing-library/react";
 import TodoForm from "./TodoForm";
 
 describe("<TodoForm />", () => {
+    const setup = (props = {}) => {
+        const utils = render(<TodoForm {...props} />);
+        const {getByText, getByPlaceholderText} = utils;
+        const input = getAllByPlaceholderText('할 일을 입력하세요'); //input이 있는지 확인
+        const button = getByText('등록'); //button 이 있는지 확인
+        return {
+            ...utils,
+            input,
+            button,
+        };
+    };
+
     it("has input and button", () => {
-        const { getByText, getByPlaceholderText } = render(<TodoForm />);
-        getByPlaceholderText("할 일을 입력하세요"); // input이 있는지 확인
-        getByText("등록"); // button이 있는지 확인
+        // const { getByText, getByPlaceholderText } = render(<TodoForm />);
+        // getByPlaceholderText("할 일을 입력하세요"); // input이 있는지 확인
+        // getByText("등록"); // button이 있는지 확인
+        const {input, button} = setup();
+        expect(input).toBeTruthy();
+        expect(button).toBeTruthy();
     });
+
     it("changes input", () => {
-        const { getByText, getByPlaceholderText } = render(<TodoForm />);
-        const input = getByPlaceholderText("할 일을 입력하세요");
+        const {input} = setup();
         fireEvent.change(input,{
             target: {
                 value: "TDD 배우기",
@@ -18,11 +33,10 @@ describe("<TodoForm />", () => {
         });
         expect(input).toHaveAttribute('value', 'TDD 배우기');
     });
+
     it("calls onInsert and clears input", () => {
         const onInsert = jest.fn();
-        const { getByText, getByPlaceholderText } = render(<TodoForm onInsert={onInsert}/>);
-        const input = getByPlaceholderText("할 일을 입력하세요");
-        const button = getByText("등록");
+        const {input, button} = setup({onInsert});
         // change 이벤트 발생시키기
         fireEvent.change(input,{
             target: {
